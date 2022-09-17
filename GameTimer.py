51064d7c5b6be1e83ffe1c141ec779960d6f140e
Frank Harvey <frankharvey88@gmail.com>
2022-09-17 11:44:13 -0400

    Update GameTimer.py

--- GameTimer.py
+++ GameTimer.py
@@ -1,4 +1,5 @@
 # from Character import Enemy
+from Character import Enemy
 
 DEFAULT_TIME_STEP = 5
 
@@ -13,18 +14,21 @@ class GameTimer:
 
         self.listeners = []
 
-    # def register_listener(self, listener: Enemy):
-    #     self.listeners.append(listener)
+    def register_listener(self, listener: Enemy):
+        self.listeners.append(listener)
 
-    def advance_time(self, time_step):
-        time_step = time_step or DEFAULT_TIME_STEP
+    def advance_time(self, elapsed_time):
+        elapsed_time = elapsed_time or DEFAULT_TIME_STEP
 
         # if action_type is not None:
         #     pass
 
         self.total_ticks += 1
-        self._game_time += time_step
-        self.last_tick_elapsed = time_step
+        self._game_time += elapsed_time
+        self.last_tick_elapsed = elapsed_time
+
+        for listener in self.listeners:
+            listener.process_time(elapsed_time)
 
     def tick(self):
         if self.player_has_control:
