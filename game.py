41ae39799eddca39b52865610ceec7da59293611
Frank Harvey <frankharvey88@gmail.com>
2022-10-08 19:32:11 -0400

    cleanup/rewriting code after Character changes
    The code for randomly placing enemies is transitional, just to test out placing multiple enemies after spending so much time with a single hard-coded enemy.  This will be fleshed out as the game engine progresses.

--- game.py
+++ game.py
@@ -1,3 +1,5 @@
+import random
+
 import arcade
 
 import constants
@@ -5,6 +7,8 @@ from ActionTypes import ActionType
 from Enemy import Enemy
 from GameTimer import GameTimer
 from Player import Player
+
+enemy_img_src = "images/zombie_idle.png"
 
 
 class Game(arcade.Window):
@@ -23,14 +27,7 @@ class Game(arcade.Window):
         self.player = None
         self.enemy = None
 
-        self.ticks = 0  # seconds
-
         self.keys_pressed = []
-
-        self.level = 1
-
-        self.score = 0
-        self.reset_score = True
 
         self.timer = GameTimer()
 
@@ -43,44 +40,52 @@ class Game(arcade.Window):
         self.tile_map = arcade.load_tilemap(map_name, constants.TILE_SCALING, layer_options=None)
 
         self.scene = arcade.Scene.from_tilemap(self.tile_map)
-        if self.reset_score:
-            self.score = 0
-        self.reset_score = True
-
         self.scene.add_sprite_list_after(constants.LAYER_NAME_PLAYER, constants.LAYER_NAME_WALLS)
         self.scene.add_sprite_list_after(constants.LAYER_NAME_ENEMIES, constants.LAYER_NAME_PLAYER)
         image_source = "images/femaleAdventurer_idle.png"
-        self.player = Player()
-        self.player.setup(10, 10, 10, 10)
-        self.player.sprite = arcade.Sprite(image_source, constants.CHARACTER_SCALING)
+        self.player = Player(image_source, constants.PLAYER_START_X, constants.PLAYER_START_Y)
+        self.player.setup(
+            strength=10,
+            dexterity=10,
+            intelligence=10,
+            constitution=10)
 
-        self.player.sprite.center_x = constants.PLAYER_START_X
-        self.player.sprite.center_y = constants.PLAYER_START_Y
-        self.scene.add_sprite(constants.LAYER_NAME_PLAYER, self.player.sprite)
+        self.scene.add_sprite(constants.LAYER_NAME_PLAYER, self.player)
 
         if self.tile_map.background_color:
             arcade.set_background_color(self.tile_map.background_color)
         else:
             arcade.set_background_color(arcade.csscolor.GRAY)
 
-        enemy_img_src = "images/zombie_idle.png"
-        self.enemy = Enemy()
-        self.enemy.setup(
+        self.add_enemy_to_scene()
+        self.center_camera_to_player()
+
+    def add_enemy_to_scene(self):
+        enemy_location = self.get_random_placement_location()
+        enemy = Enemy(enemy_img_src, enemy_location[0], enemy_location[1])
+        enemy.setup(
             strength=10,
             dexterity=10,
-            intelligence=10, constitution=10,
+            intelligence=10,
+            constitution=10,
             health=5,
             mana=5,
             scene=self.scene,
             target=self.player
         )
-        self.enemy.sprite = arcade.Sprite(enemy_img_src, constants.CHARACTER_SCALING)
-        self.enemy.sprite.center_x = constants.ENEMY_START_X
-        self.enemy.sprite.center_y = constants.ENEMY_START_Y
-        self.scene.add_sprite(constants.LAYER_NAME_ENEMIES, self.enemy.sprite)
-        self.timer.register_listener(self.enemy)
 
-        self.center_camera_to_player()
+        self.scene.add_sprite(constants.LAYER_NAME_ENEMIES, enemy)
+        self.timer.register_listener(enemy)
+
+    def get_random_placement_location(self):
+        map_width = self.tile_map.tiled_map.map_size.width
+        map_height = self.tile_map.tiled_map.map_size.height
+        # tile_size = self.tile_map.tiled_map.tile_size.width  # we ensure width and height are equal
+
+        x = (random.randint(0, map_width) * constants.SCALED_SPRITE_PIXEL_SIZE) + (16 * constants.TILE_SCALING)
+        y = (random.randint(0, map_height) * constants.SCALED_SPRITE_PIXEL_SIZE) - (16 * constants.TILE_SCALING)
+
+        return x, y
 
     def on_draw(self):
         """Render the screen."""
@@ -125,33 +130,36 @@ class Game(arcade.Window):
     def process_movement(self):
         (move_x, move_y) = self.player.get_movement_direction(self.keys_pressed)
 
-        previous_location = [self.player.sprite.center_x, self.player.sprite.center_y]
-        self.player.sprite.set_position(
-            self.player.sprite.center_x + (move_x * constants.PLAYER_MOVEMENT_SPEED),
-            self.player.sprite.center_y + (move_y * constants.PLAYER_MOVEMENT_SPEED)
+        previous_location = [self.player.center_x, self.player.center_y]
+        self.player.set_position(
+            self.player.center_x + (move_x * constants.PLAYER_MOVEMENT_SPEED),
+            self.player.center_y + (move_y * constants.PLAYER_MOVEMENT_SPEED)
         )
 
-        wall_hit_list = arcade.check_for_collision_with_lists(self.player.sprite,
+        wall_hit_list = arcade.check_for_collision_with_lists(self.player,
                                                               [self.scene[constants.LAYER_NAME_FOREGROUND],
                                                                self.scene[constants.LAYER_NAME_WALLS]])
-        enemy_hit_list = arcade.check_for_collision_with_list(self.player.sprite,
+        enemy_hit_list = arcade.check_for_collision_with_list(self.player,
                                                               self.scene[constants.LAYER_NAME_ENEMIES])
 
         action_type = ActionType.MOVE if move_x == 0 or move_y == 0 else ActionType.MOVE_DIAGONAL
         if len(wall_hit_list) > 0:
-            self.player.sprite.set_position(previous_location[0], previous_location[1])
+            self.player.set_position(previous_location[0], previous_location[1])
         elif len(enemy_hit_list) > 0:
-            self.player.sprite.set_position(previous_location[0], previous_location[1])
+            self.player.set_position(previous_location[0], previous_location[1])
             action_type = ActionType.ATTACK
-            self.enemy.health -= 1
+            enemy_hit_list[0].health -= 1
 
         action_time = self.player.calculate_action_time(constants.MOVEMENT_TIMES[action_type])
         if action_time > 0:
             self.timer.advance_time(action_time)
+            if self.timer.total_ticks % 60 == 0:
+                self.add_enemy_to_scene()
 
-            if self.enemy.health <= 0:
-                self.timer.unregister_listener(self.enemy)
-                self.enemy.sprite.kill()
+            for enemy in self.scene[constants.LAYER_NAME_ENEMIES]:
+                if enemy.health <= 0:
+                    self.timer.unregister_listener(enemy)
+                    enemy.kill()
 
     def on_update(self, delta_time: float):
         if self.player.is_moving and (self.player.move_wait_elapsed >= self.player.move_delay):
@@ -166,8 +174,8 @@ class Game(arcade.Window):
             self.player.move_wait_elapsed += delta_time
 
     def center_camera_to_player(self):
-        screen_center_x = self.player.sprite.center_x - (self.camera.viewport_width / 2)
-        screen_center_y = self.player.sprite.center_y - (self.camera.viewport_height / 2)
+        screen_center_x = self.player.center_x - (self.camera.viewport_width / 2)
+        screen_center_y = self.player.center_y - (self.camera.viewport_height / 2)
 
         screen_center_x = max(screen_center_x, 0)
         screen_center_y = max(screen_center_y, 0)
