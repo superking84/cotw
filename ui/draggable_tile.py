63bb7c9b52aade2148846abe08c8e2738f0f48ac
Frank Harvey <frankharvey88@gmail.com>
2023-04-23 15:38:36 -0500

    Update draggable_tile.py
    simplified initialization code

--- ui/draggable_tile.py
+++ ui/draggable_tile.py
@@ -1,11 +1,7 @@
-from typing import Optional
-
 from arcade.gui import UISpriteWidget, UIWindowLikeMixin
 from arcade.sprite import Sprite
 
 
 class DraggableTile(UISpriteWidget, UIWindowLikeMixin):
-    def __init__(self, x: float = 0, y: float = 0, width: float = 48, height: float = 80,
-                 sprite: Optional[Sprite] = None, size_hint=None, size_hint_min=None, size_hint_max=None, style=None):
-        super(DraggableTile, self).__init__(x=x, y=y, width=width, height=height, sprite=sprite, size_hint=size_hint,
-                                            size_hint_min=size_hint_min, size_hint_max=size_hint_max, style=style)
+    def __init__(self, sprite: Sprite):
+        super().__init__(x=sprite.left, y=sprite.bottom, width=sprite.width, height=sprite.height, sprite=sprite)
