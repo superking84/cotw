813d917927ac91bc4e3430d5e267c8ffbcb24da6
Frank Harvey <frankharvey88@gmail.com>
2023-04-24 17:40:34 -0500

    added improved logic for draggable item tiles

--- ui/draggable_tile.py
+++ ui/draggable_tile.py
@@ -1,7 +1,15 @@
-from arcade.gui import UISpriteWidget, UIWindowLikeMixin
+from arcade.gui import UISpriteWidget
 from arcade.sprite import Sprite
+
+from ui.mixins import UIWindowLikeMixin
 
 
 class DraggableTile(UISpriteWidget, UIWindowLikeMixin):
     def __init__(self, sprite: Sprite):
         super().__init__(x=sprite.left, y=sprite.bottom, width=sprite.width, height=sprite.height, sprite=sprite)
+
+        # self.is_dragging = False
+        self.total_dx = 0
+        self.total_dy = 0
+        self.home_x = sprite.center_x
+        self.home_y = sprite.center_y
--- ui/inventory_ui_manager.py
+++ ui/inventory_ui_manager.py
@@ -1,5 +1,6 @@
 import arcade.gui
 
+from ui.draggable_tile import DraggableTile
 from ui.inventory_slot_widget import InventorySlotWidget
 
 
@@ -7,11 +8,25 @@ class InventoryUIManager(arcade.gui.UIManager):
     def __init__(self):
         super().__init__()
 
+        self.dragged_tile: DraggableTile = None
+
     def on_event(self, event) -> bool:
+        if isinstance(event, arcade.gui.UIMousePressEvent):
+            for widget in self.get_widgets_at(event.pos):
+                if isinstance(widget, DraggableTile):
+                    self.dragged_tile = widget
+                    break
+
+        if isinstance(event, arcade.gui.events.UIMouseDragEvent):
+            if self.dragged_tile is not None:
+                self.dragged_tile.total_dx += event.dx
+                self.dragged_tile.total_dy += event.dy
         if isinstance(event, arcade.gui.UIMouseReleaseEvent):
-            mouse_event: arcade.gui.UIMouseReleaseEvent = event
-            for widget in self.get_widgets_at(mouse_event.pos):
-                if isinstance(widget, InventorySlotWidget):
-                    print(widget.name)
+            if self.dragged_tile is not None:
+                if not any([isinstance(widget, InventorySlotWidget) for widget in self.get_widgets_at(event.pos)]):
+                    self.dragged_tile.move(-self.dragged_tile.total_dx, -self.dragged_tile.total_dy)
+                self.dragged_tile.total_dx = 0
+                self.dragged_tile.total_dy = 0
+                self.dragged_tile = None
 
         return super().on_event(event)
