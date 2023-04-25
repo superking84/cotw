a2cbfca18a5b8c7617e3b343a5bef64323778d5d
Frank Harvey <frankharvey88@gmail.com>
2023-04-25 08:24:26 -0500

    Update item.py
    added name field, removed wear_location field as an Item does not need to know where it's being held (the player inventory will track that)

--- game_objects/item.py
+++ game_objects/item.py
@@ -1,4 +1,4 @@
-from typing import List, Optional
+from typing import List
 
 import arcade
 
@@ -11,11 +11,11 @@ class Item:
     Any in-game object that the player may carry and interact with in some way.
     """
 
-    def __init__(self, img_src: str, center_x: int, center_y: int):
+    def __init__(self, name: str, img_src: str, center_x: int, center_y: int):
         self.sprite = arcade.Sprite(img_src, constants.CHARACTER_SCALING, center_x=center_x, center_y=center_y)
 
+        self.name = name
         self.weight = 0
         self.size = 0
 
         self.valid_wear_locations: List[WearLocation] = [WearLocation.LEFT_HAND, WearLocation.RIGHT_HAND]
-        self.wear_location: Optional[WearLocation] = None
