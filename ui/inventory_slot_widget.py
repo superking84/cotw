fe21bbdbe60de664006df3d4beac9ff790f98cdc
Frank Harvey <frankharvey88@gmail.com>
2023-04-29 09:57:19 -0500

    Update inventory_slot_widget.py
    added wear_location prop to InventorySlotWidget

--- ui/inventory_slot_widget.py
+++ ui/inventory_slot_widget.py
@@ -3,11 +3,12 @@ from typing import Optional
 from arcade.gui import UIWidget
 
 from game_objects.item import Item
+from utils.enums import WearLocation
 
 
 class InventorySlotWidget(UIWidget):
-    def __init__(self, x: float, y: float, width: float, height: float, name: str):
+    def __init__(self, x: float, y: float, width: float, height: float, wear_location: WearLocation):
         super().__init__(x, y, width, height)
 
-        self.name = name
+        self.wear_location: WearLocation = wear_location
         self.item: Optional[Item] = None  # the item being held in the inventory slot
