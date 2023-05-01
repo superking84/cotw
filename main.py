ac3e67556c63b9f8fbf562b4b13ae528a8cdb47f
Frank Harvey <frankharvey88@gmail.com>
2023-04-30 19:04:46 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import arcade
 
 import game
+from db.db_manager import DBManager
 
 if __name__ == "__main__":
+    DBManager.generate_schema()
+    
     game = game.Game()
     game.show_view(game.views["world"])
 
